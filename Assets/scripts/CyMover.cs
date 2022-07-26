fea6a60c27c7d0d556c76afddc479666cd42f8d2
asala00 <asalastrying@gmail.com>
2022-07-26 18:56:10 +0300

    camera update
    fixed camera script to take mouse input & rigged it for perfect player view + now player moves where the cam is pointing not to the local rotation of the game world

--- Assets/scripts/CyMover.cs
+++ Assets/scripts/CyMover.cs
@@ -3,37 +3,21 @@ using UnityEngine;
 
 public class CyMover : MonoBehaviour
 {
-    //private int rotSpeed = 200;
+   
     public CharacterController _controller;
     public float _speed = 5f;
-    
+    //ref for our cam so we can use it to change where our GO's forward is (the way the camera is facing)
+    public Transform cam;
    
     
     void Start()
     {
-        // controller = GetComponent<CharacterController>();  
+       
     }
 
 
     void Update()
     {
-        // float horizInput = Input.GetAxisRaw("Horizontal");
-        // float verticInput = Input.GetAxisRaw("Vertical");
-        //
-        // Vector3 movDirec = new Vector3(horizInput, 0, verticInput);
-        //
-        // float magnitude = Mathf.Clamp01(movDirec.magnitude) * speed;
-        // movDirec.Normalize();
-        //
-        // //SimpleMove method have time.delta built in
-        // controller.SimpleMove(movDirec * magnitude);
-        //
-        //
-        // if (movDirec != Vector3.zero)
-        // {
-        //     Quaternion torotate = Quaternion.LookRotation(movDirec, Vector3.up);
-        //     transform.rotation = Quaternion.RotateTowards(transform.rotation, torotate, rotSpeed * Time.deltaTime);
-        // }
         float horizInput = Input.GetAxisRaw("Horizontal") * _speed;
         float verticInput = Input.GetAxisRaw("Vertical") * _speed;
 
@@ -42,14 +26,18 @@ public class CyMover : MonoBehaviour
         //telling the player to move according to the input
         if (_direction.magnitude >= 0.1f)
         {
-            //to point our player facing forward with each turn (aka rotation) + the * by radius is cuz the atan will return a radius value
-            float _targetAngel = Mathf.Atan2(_direction.x, _direction.z) * Mathf.Rad2Deg;
+            //to point our player facing forward with each turn (aka rotation)
+            //+ the * by radius is cuz the atan will return a radius value
+            //used cam.eulerAngles.y to add it the targets angle by the cameras
+            float _targetAngel = Mathf.Atan2(_direction.x, _direction.z) * Mathf.Rad2Deg+cam.eulerAngles.y;
             //setting our rotation according to the previous line of code
             transform.rotation = Quaternion.Euler(0f, _targetAngel, 0f);
 
-            //changes the GO 
-          Vector3 final_di = transform.TransformDirection( _direction );
-            _controller.Move(_direction  * _speed * Time.deltaTime );
+            
+            //changes the GO dierction istead of just rotation
+            Vector3 moveDirection = Quaternion.Euler(0f, _targetAngel, 0f) * Vector3.forward;
+            
+            _controller.Move(moveDirection.normalized * _speed * Time.deltaTime );
           //  Debug.Log($"Move Vector: {_direction} -- Transformed Vector: {final_di}");
 
         }
