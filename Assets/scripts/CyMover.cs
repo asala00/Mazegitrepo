1d59aaafdb41568a6842cd94ac698acf8cfccc1c
asala00 <asalastrying@gmail.com>
2022-07-27 16:43:35 +0300

    updated colliders and triggers
    set up the coins as triggers and the exit wall as collider

--- Assets/scripts/CyMover.cs
+++ Assets/scripts/CyMover.cs
@@ -1,20 +1,23 @@
 
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CyMover : MonoBehaviour
 {
-   
+
     public CharacterController _controller;
-    public float _speed = 5f;
+
+    [SerializeField] public float _speed = 5.0f;
+
     //ref for our cam so we can use it to change where our GO's forward is (the way the camera is facing)
     public Transform cam;
-    
+
     void Start()
     {
-       
+
     }
-    
+
     void Update()
     {
         float horizInput = Input.GetAxisRaw("Horizontal") * _speed;
@@ -28,30 +31,51 @@ public class CyMover : MonoBehaviour
             //to point our player facing forward with each turn (aka rotation)
             //+ the * by radius is cuz the atan will return a radius value
             //used cam.eulerAngles.y to add it the targets angle by the cameras
-            float _targetAngel = Mathf.Atan2(_direction.x, _direction.z) * Mathf.Rad2Deg+cam.eulerAngles.y;
+            float _targetAngel = Mathf.Atan2(_direction.x, _direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             //setting our rotation according to the previous line of code
             transform.rotation = Quaternion.Euler(0f, _targetAngel, 0f);
             //changes the GO dierction istead of just rotation
             Vector3 moveDirection = Quaternion.Euler(0f, _targetAngel, 0f) * Vector3.forward;
-            
-            _controller.Move(moveDirection.normalized * _speed * Time.deltaTime );
-          
+
+            _controller.Move(moveDirection.normalized * _speed * Time.deltaTime);
+
+
+
+
 
         }
-        
+
     }
-    
+
     //creating a list to store the amount of coins collected so we can use the amount to open the door later
-    [SerializeField] private int coinAmount = 0;
+
+    [SerializeField] 
+    public int coinAmount = 0;
+    [SerializeField]
     private GameObject myCoin;
-    void OnCollisionEnter( Collision myCoin )
+    // void OnTriggerEnter ( Collider myCoin )
+    // {
+    //    
+
+    private void OnTriggerEnter(Collider myCoin)
     {
+
+
         if (myCoin.gameObject.CompareTag("coin"))
         {
+            _speed += 1;
             coinAmount += 1; // saves the coin amount in the list
             Destroy(myCoin.gameObject);
         }
     }
-    
+
+    [SerializeField] private GameObject exit;
+    private void OnControllerColliderHit (ControllerColliderHit exit)
+    {
+        if (coinAmount == 3 && exit.gameObject.CompareTag("exit"))
+        {
+            Destroy(exit.gameObject);
+        }
+    }
 }
 
